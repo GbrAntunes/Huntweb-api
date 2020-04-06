b23d89fb91ae62f642c48b435128162518a2d804
Gabriel Antunes <gbr.antunes@gmail.com>
2020-04-05 23:34:08 -0400

    Crud concluído.

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const requireDir = require('require-dir') // Biblioteca para usar require em um dir
 
 // Inicializando o app
 const app = express()
 
+// Define que o envio de dados para a api será feito em formato JSON
+app.use(express.json())
+
 // Iniciando o DB
 mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true })
-require('./src/models/Products')
+requireDir('./src/models') // Importando todas as models
 
-// Rotas
-app.get('/', (request, response) => {
-
-})
+app.use('/api', require('./src/routes'))
 
 app.listen(3000)
